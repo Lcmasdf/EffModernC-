e2e983efde97d764283e365d16d8baca6ea14b52
lichengmin <lichengmin@sensetime.com>
2019-08-21 22:08:08 +0800

    fix async_task

--- concurrency/async_task.hpp
+++ concurrency/async_task.hpp
@@ -1,22 +1,22 @@
 #include <future>
 #include <iostream>
-#include <windows.h>
+#include <unistd.h>
 
 using namespace std;
 int sleep_and_add(int n) {
     int ret = 0;
     for (int i = 0 ; i < n; i++) {
-        Sleep(i);
+        sleep(i);
         ret += i;
     }
     return ret;
 }
 
 void test() {
-    auto func1 = std::async(std::launch::async, sleep_and_add, 3);
+    auto func1 = std::async(std::launch::deferred, sleep_and_add, 3);
     auto func2 = std::async(std::launch::async, sleep_and_add, 3);
 
-    Sleep(2);
+    sleep(2);
     func1.get();
     func2.get();
     return ;
