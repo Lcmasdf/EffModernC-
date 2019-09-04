45f894bf6d934ca8fc112ef7a77e89a8ed0ddc00
lichengmin <lichengmin@126.com>
2019-09-04 09:00:16 +0800

    update

--- auto_ptr/shared_ptr.hpp
+++ auto_ptr/shared_ptr.hpp
@@ -6,37 +6,13 @@
 // 5.sp_count_deleter
 // 6.sp_count_ptr_inplace
 // **template
+#include <utility>
 
 class __shared_ptr_int;
 class deleter_int {};
 class sp_count_ptr;
 class sp_count_base;
 typedef void (*DeleterFunc)(int*);
-
-class shared_ptr_int : public __shared_ptr_int {
-   public:
-    shared_ptr_int(decltype(nullptr), DeleterFunc = nullptr) noexcept;
-    explicit shared_ptr_int(int, DeleterFunc = nullptr) noexcept;
-    ~shared_ptr_int();
-
-    shared_ptr_int(const shared_ptr_int&) noexcept;
-    shared_ptr_int(shared_ptr_int&&) noexcept;
-
-    shared_ptr_int operator=(const shared_ptr_int&) noexcept;
-    shared_ptr_int operator=(shared_ptr_int&&) noexcept;
-};
-
-// shared_ptr_int::shared_ptr_int(decltype(nullptr) _ptr,)
-shared_ptr_int::shared_ptr_int(int val, DeleterFunc _del)
-    : __shared_ptr_int(val, _del) {}
-
-shared_ptr_int::shared_ptr_int(const shared_ptr_int& rhs): __shared_ptr_int(rhs){}
-
-shared_ptr_int::shared_ptr_int(shared_ptr_int&& lrhs): __shared_ptr_int(lrsh){}
-
-shared_ptr_int shared_ptr_int::operator=(const shared_ptr_int& rhs){
-    return __shared_ptr_int::operator=(rhs);
-}
 
 class __shared_ptr_int {
     int* _ptr;
@@ -49,18 +25,27 @@ class __shared_ptr_int {
     __shared_ptr_int(const __shared_ptr_int&) noexcept;
     __shared_ptr_int(__shared_ptr_int&&) noexcept;
 
-    __shared_ptr_int operator=(const __shared_ptr_int&) noexcept;
-    __shared_ptr_int operator=(__shared_ptr_int&&) noexcept;
+    __shared_ptr_int& operator=(const __shared_ptr_int&) noexcept;
+    __shared_ptr_int& operator=(__shared_ptr_int&&) noexcept;
     int operator*() noexcept;
     int operator->() noexcept;
 };
 
-__shared_ptr_int::__shared_ptr_int() : _ptr(nullptr), _Sp_count(nullptr) {}
+class shared_ptr_int : public __shared_ptr_int {
+   public:
+    shared_ptr_int(decltype(nullptr), DeleterFunc = nullptr) noexcept;
+    explicit shared_ptr_int(int, DeleterFunc = nullptr) noexcept;
+    ~shared_ptr_int();
 
-__shared_ptr_int::__shared_ptr_int(int val, DeleterFunc del) {
-    _ptr = new int(val);
-    _Sp_count = new sp_count_ptr(_ptr, del);
-}
+    shared_ptr_int(const shared_ptr_int&) noexcept;
+    shared_ptr_int(shared_ptr_int&&) noexcept;
+
+    shared_ptr_int& operator=(const shared_ptr_int&) noexcept;
+    shared_ptr_int& operator=(shared_ptr_int&&) noexcept;
+
+    int operator*() noexcept;
+    int operator->() noexcept;
+};
 
 class sp_count_base {
     int* _ptr;
@@ -85,6 +70,75 @@ class sp_count_base {
     void deleter(int*);
 };
 
+// shared_ptr_int::shared_ptr_int(decltype(nullptr) _ptr,)
+shared_ptr_int::shared_ptr_int(int val, DeleterFunc _del)
+    : __shared_ptr_int(val, _del) {}
+
+shared_ptr_int::shared_ptr_int(const shared_ptr_int& rhs): __shared_ptr_int(rhs){}
+
+shared_ptr_int::shared_ptr_int(shared_ptr_int&& lrhs): __shared_ptr_int(std::move(lrhs)){}
+
+shared_ptr_int& shared_ptr_int::operator=(const shared_ptr_int& rhs){
+    this->__shared_ptr_int::operator=(rhs);
+    return *this;
+}
+
+shared_ptr_int& shared_ptr_int::operator=(shared_ptr_int&& lrhs) {
+    this->__shared_ptr_int::operator=(std::move(lrhs));
+    return *this;
+}
+
+int shared_ptr_int::operator*() {
+    return this->operator*();
+}
+
+__shared_ptr_int::__shared_ptr_int() : _ptr(nullptr), _Sp_count(nullptr) {}
+
+__shared_ptr_int::__shared_ptr_int(int val, DeleterFunc del) {
+    _ptr = new int(val);
+    _Sp_count = new sp_count_ptr(_ptr, del);
+}
+
+__shared_ptr_int::__shared_ptr_int(const __shared_ptr_int& rhs): _ptr(rhs._ptr) {
+    _Sp_count->dispose();
+    _Sp_count = rhs._Sp_count;
+    _Sp_count->add_ref();
+}
+
+__shared_ptr_int::__shared_ptr_int(__shared_ptr_int&& lrhs) {
+    _ptr = lrhs._ptr;
+    lrhs._ptr = nullptr;
+    _Sp_count = lrhs._Sp_count;
+    lrhs._Sp_count = nullptr;
+}
+
+__shared_ptr_int& __shared_ptr_int::operator=(const __shared_ptr_int& rhs) {
+    _ptr = rhs._ptr;
+    _Sp_count->dispose();
+    _Sp_count = rhs._Sp_count;
+    _Sp_count->add_ref();
+    return *this;
+}
+
+__shared_ptr_int& __shared_ptr_int::operator=(__shared_ptr_int&& lrhs) {
+    _ptr = lrhs._ptr;
+    lrhs._ptr = nullptr;
+    _Sp_count = lrhs._Sp_count;
+    lrhs._Sp_count = nullptr;
+    return *this;
+}
+
+int __shared_ptr_int::operator*() {
+    if (_ptr != nullptr) {
+        return *_ptr;
+    }
+    return 0;
+}
+
+int __shared_ptr_int::operator->() {
+    return operator*();
+}
+
 class sp_count_ptr : public sp_count_base {
    public:
     sp_count_ptr();
@@ -97,16 +151,6 @@ sp_count_ptr::sp_count_ptr(int* _ptr, DeleterFunc _del)
 
 sp_count_base::sp_count_base(int* ptr, DeleterFunc del)
     : _ptr(ptr), _del(del), _use_count(1), _weak_count(0) {}
-
-// sp_count_base::sp_count_base(const sp_count_base& spb)
-//     : _ptr(spb._ptr),
-//       _del(spb._del),
-//       _use_count(spb._use_count),
-//       _weak_count(spb._weak_count) {}
-
-// sp_count_base::sp_count_base(sp_count_base&&) {
-
-// }
 
 void sp_count_base::deleter(int* _ptr) {
     if (_del == nullptr) {
